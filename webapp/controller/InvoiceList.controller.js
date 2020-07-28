7c38279757f9dd3bb4c7dde34a2c5c8fa956a0cf
Alexandru Gavrilas <alexandru.gavrilas@mhp.com>
2020-07-28 10:27:00 +0300

    step 23

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter"
+], function (Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("org.ubb.hw.controller.InvoiceList", {
+		formatter: formatter,
 
 		onInit : function () {
 			var oViewModel = new JSONModel({
